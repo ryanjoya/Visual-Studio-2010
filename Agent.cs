f141a441932d81b96df90b6d470b57106584fc5c
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-08-27 12:54:08 -0500

    Create Agent.cs

---
+++ Agent.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeworkDay4
+{
+    class Agent : Asset
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Agent Print Method");
+            Console.ReadLine();
+        }
+
+    }
+}
