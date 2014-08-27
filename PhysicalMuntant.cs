5ed987dece95cea2f2869e7b5bbfab29f8f67efe
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-08-27 12:39:02 -0500

    Create PhysicalMuntant.cs

---
+++ PhysicalMuntant.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day5Homework
+{
+    class PhysicalMutant
+    {
+        public int IQ;
+        public int Strength;
+
+    }
+}
