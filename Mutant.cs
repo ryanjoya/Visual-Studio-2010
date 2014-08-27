126ac74fdfb06d6101af535584fb344543961606
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-08-27 12:39:28 -0500

    Create Mutant.cs

---
+++ Mutant.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day5Homework
+{
+    class Mutant
+    {
+        string CodeName;
+        int Level;
+
+        public abstract void DangerQuotient();
+
+    }
+}
