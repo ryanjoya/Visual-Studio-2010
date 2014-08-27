71ede1e62ed5d23c25919271ccd98d29fa43d346
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-08-27 12:39:17 -0500

    Create PsychicMutant.cs

---
+++ PsychicMutant.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day5Homework
+{
+    class PsychicMutant
+    {
+        public int IQ;
+        public int UsageCount;
+
+    }
+}
