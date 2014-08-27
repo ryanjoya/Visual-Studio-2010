e7e829ddc0f30435960b27450fd7c0d154e9a608
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-08-27 12:51:35 -0500

    Create Agency.cs

---
+++ Agency.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Global_Spy_Analysis_Application_Class_Diagram
+{
+    class Agency
+    {
+        public string AgencyName;
+        public string Country;
+        public int OrganizationSize;
+
+        public void GetAgencyInfo()
+        {
+
+        }
+    }
+}
